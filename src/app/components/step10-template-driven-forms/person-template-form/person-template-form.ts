8054e7badc1a7edada9495b71fbb37cbb31ea1d6
Iliaskokkal <kokkiliasgrandera@gmail.com>
2026-04-28 20:05:08 +0300

    generated step10 form

---
+++ src/app/components/step10-template-driven-forms/person-template-form/person-template-form.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-person-template-form',
+  imports: [],
+  templateUrl: './person-template-form.html',
+  styleUrl: './person-template-form.css',
+})
+export class PersonTemplateForm {}
---
+++ src/app/components/step10-template-driven-forms/step10-template-driven-forms.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-step10-template-driven-forms',
+  imports: [],
+  templateUrl: './step10-template-driven-forms.html',
+  styleUrl: './step10-template-driven-forms.css',
+})
+export class Step10TemplateDrivenForms {}
